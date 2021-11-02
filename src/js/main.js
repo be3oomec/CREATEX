9a5fb66c4a912e6d266fed3a8c41bfc2fa1bcec2
be3oomec <rustikmuzipov@gmail.com>
2021-11-02 17:33:02 +0500

    added header

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 
-  const cartBtn = document.querySelector('.header__btn--cart');
-  const miniCart = document.querySelector('.mini-cart');
+  // const cartBtn = document.querySelector('.header__btn--cart');
+  // const miniCart = document.querySelector('.mini-cart');
 
-  cartBtn.addEventListener('click', () => {
-    miniCart.classList.add('mini-cart--visible');
-  });
+  // cartBtn.addEventListener('click', () => {
+  //   miniCart.classList.add('mini-cart--visible');
+  // });
 
-  document.addEventListener('click', (e) => {
-    if (!e.target.classList.contains('mini-cart') && !e.target.classList.contains('header__btn--cart')) {
-      miniCart.classList.remove('mini-cart--visible');
-    }
-  });
+  // document.addEventListener('click', (e) => {
+  //   if (!e.target.classList.contains('mini-cart') && !e.target.classList.contains('header__btn--cart')) {
+  //     miniCart.classList.remove('mini-cart--visible');
+  //   }
+  // });
